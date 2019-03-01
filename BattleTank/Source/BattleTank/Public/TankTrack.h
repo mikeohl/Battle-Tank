bf72a394f7cb45c64aa153240355237073831572
Michael Ohl <mikeohl@live.com>
2019-03-01 02:36:08 -0800

    Set trigger axis to control tank track throttle and created Track class
    TankTrack class has blueprintcallable methods to set throttle in blueprint with trigger axes

---
+++ BattleTank/Source/BattleTank/Public/TankTrack.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/StaticMeshComponent.h"
+#include "TankTrack.generated.h"
+
+/**
+ * Tank Track Class sets maximum driving force and can apply force for tank movement
+ */
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent), hidecategories = ("Collision"))
+class BATTLETANK_API UTankTrack : public UStaticMeshComponent
+{
+	GENERATED_BODY()
+	
+public:
+	UFUNCTION(BlueprintCallable, Category = Input)
+	void SetThrottle(float Throttle);
+	
+	
+};
